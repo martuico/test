08e4a8fbe356fa2aa79953f3ee2f330681cc98e1
Mar Tuico <mar.tuico@gmail.com>
2015-09-07 20:23:18 +0800

    done

--- app.js
+++ app.js
@@ -14,18 +14,41 @@ angular.module('myApp', [
 	.controller('loginController', function($scope) {
 		$scope.datas = [];
 		var now = new Date();
-		$scope.currentTime = (now.getHours() > 12) ?  (now.getHours() - 12).pad() + ':' + now.getMinutes()
-							: now.getHours().pad() + ':' + now.getMinutes();
-	  	$scope.add = function(){
-	  		$scope.datas.push({login: $scope.currentTime,logout:''});
+		$scope.datenow = now.getMonth().pad() +'/' + now.getDay().pad() + '/' + now.getFullYear();
+		$scope.currentTime = now.getHours().pad() + ':' + now.getMinutes().pad();
+		$scope.datas.push({login: $scope.currentTime,logout:'',totaltime:''});
+
+		$scope.totalhours = function(){
+			// $('.totaltime').each(function(){
+
+			// });
 		}
 
-		$scope.store = function(data){
-			console.log(data);
+	  	$scope.add = function(){
+	  		$scope.datas.push({login: $scope.currentTime,logout:'',totaltime:''});
+		}
+		$scope.store = function(data,index){
+			var totaltime = tolalHours(data.login,data.logout);
+			$scope.datas[index].totaltime = totaltime;
+			var values = $scope.datas;
+			var minutes = [];
+			var hours = [];
+			angular.forEach(values, function(value, key){
+			  var re = value.totaltime.replace(' hours','').replace(' and ',':').replace(' minutes','').replace(' ','');
+
+			 	re = re.split(':');
+			 	minutes.push(re[1]);
+			 	hours.push(re[0]);
+			});
+
+			if((parseInt(minutes.sum()) / 60) > 0){
+				hours.push(parseInt(minutes.sum()) / 60);
+			}
+
+			$scope.totalhours = hours.sum();
 		}
 
 		$scope.remove = function(data,index){
-
 			$scope.datas.splice(index, 1);
 		}
 	})
--- custom.js
+++ custom.js
@@ -6,8 +6,40 @@ function remove(arr, item) {
       }
   }
 
+  Array.prototype.sum = function () {
+      var total = 0;
+      var i = this.length;
+
+      while (i--) {
+          total += parseInt(this[i]);
+      }
+
+      return total;
+  }
+
+Array.prototype.newsum = function(){
+	this.reduce(function(pv, cv) { return pv + cv; }, 0);
+}
+
 Number.prototype.pad = function(size) {
       var s = String(this);
       while (s.length < (size || 2)) {s = "0" + s;}
       return s;
-    }
+}
+ function tolalHours(login,logout){
+ 	if(login == '' || login.length < 1 || typeof login != 'string' &&
+ 	  logout == '' ||logout.length < 1 || typeof logout != 'string'){
+ 		return '00:00';
+ 	}
+	var login = login.split(':');
+	var logout = logout.split(':');
+	var inhour = parseInt(login[0]);
+	var inminute = parseInt(login[1]);
+	var outhour = parseInt(logout[0]);
+	var outminute = parseInt(logout[1]);
+	var totalloginminutes = inhour + inminute;
+	// var totallogoutnminutes = outhour + outminute;
+	var hour = ""+  outhour - inhour +" hours";
+	var minutes = ""+  outminute - inminute + " minutes" ;
+	return hour +' and '+minutes;
+}
